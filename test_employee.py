c3660074a014428cc011bedf2506c7eb54fea87f
Tomisin <tomisin@xc0de>
2023-05-23 16:57:52 +0300

    untrack __pycache__

--- test_employee.py
+++ test_employee.py
@@ -3,22 +3,30 @@ import unittest
 
 class TestEmpl(unittest.TestCase):
 
-    def test_email(self):
-        emp_1 = Employee('Tomi', 'Sin', 6000)
-        emp_2 = Employee('De', 'Borah', 6000)
+    def setUp(self) -> None:
+        print("Setup")
+        self.emp_1 = Employee('Tomi', 'Sin', 6000)
+        self.emp_2 = Employee('De', 'Borah', 6000)
 
-        self.assertEqual(emp_1.email, 'Tomi.Sin@email.com')
-        self.assertEqual(emp_2.email, 'De.Borah@email.com')
+    def tearDown(self) -> None:
+        print("tearDown\n")
+
+    def test_email(self):
+        self.assertEqual(self.emp_1.email, 'Tomi.Sin@email.com')
+        self.assertEqual(self.emp_2.email, 'De.Borah@email.com')
 
 
     def test_fullname(self):
-        emp_1 = Employee('Tomi', 'Sin', 6000)
-        emp_2 = Employee('De', 'Borah', 6000)
-
-        self.assertEqual(emp_1.fullname, 'Tomi Sin')
-        self.assertEqual(emp_2.fullname, 'De Borah')
+        self.assertEqual(self.emp_1.fullname, 'Tomi Sin')
+        self.assertEqual(self.emp_2.fullname, 'De Borah')
 
 
+    def test_apply_raise(self):
+        self.emp_1.apply_raise()
+        self.emp_2.apply_raise()
+
+        self.assertEqual(self.emp_1.pay, 6300)
+        self.assertEqual(self.emp_2.pay, 6300)
 
 
 
